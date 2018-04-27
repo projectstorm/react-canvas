cb9c24e95313cb65cfb7ff32d05aa892184b23d9
Dylan Vorster <dylanvorster.dv@gmail.com>
2018-04-27 15:14:45 +0200

    Testing needs to be deterministic, so allow for deterministic UID

--- src/Toolkit.ts
+++ src/Toolkit.ts
@@ -1,9 +1,15 @@
 export class Toolkit {
+	static TESTING: boolean = false;
+	static TESTING_UID = 0;
+
 	/**
-	 * Generats a unique ID (thanks Stack overflow :3)
-	 * @returns {String}
+	 * Generates a unique ID (thanks Stack overflow :3)
 	 */
 	public static UID(): string {
+		if (Toolkit.TESTING) {
+			Toolkit.TESTING_UID++;
+			return "" + Toolkit.TESTING_UID;
+		}
 		return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
 			const r = (Math.random() * 16) | 0;
 			const v = c === "x" ? r : (r & 0x3) | 0x8;
