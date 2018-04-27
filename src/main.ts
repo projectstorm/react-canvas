c0ab833d4d7637df9639bc16281a8463712eb4d2
Dylan Vorster <dylanvorster.dv@gmail.com>
2018-04-27 15:08:20 +0200

    export the stuffs and add required package json stuff

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+export * from "./CanvasEngine";
+export * from "./Toolkit";
+export * from "./AbstractElementFactory";
+export * from "./CanvasLayerFactory";
+
+export * from "./widgets/BaseWidget";
+export * from "./widgets/CanvasLayerWidget";
+export * from "./widgets/AnchorWidget";
+export * from "./widgets/SelectionGroupWidget";
+
+export * from "./geometry/Point";
+export * from "./geometry/Polygon";
+export * from "./geometry/Rectangle";
+
+export * from "./models/BaseObject";
+export * from "./models/BaseModel";
+export * from "./models/GraphModel";
+
+export * from "./history/HistoryBank";
+
+export * from "./event-bus/Action";
+export * from "./event-bus/InlineAction";
+export * from "./event-bus/Event";
+export * from "./event-bus/EventBus";
